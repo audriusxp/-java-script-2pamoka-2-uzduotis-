6663cf6046393904e32c68eec4e9a9656673cbb2
audriusxp <palm@inbox.lt>
2022-06-09 09:21:40 +0300

    atlikta pirma dalis uzduoties

--- script.js
+++ script.js
@@ -1,3 +1,14 @@
 window.onload = function() {
+  let zodis = "kelmas";
+  let sujungtiZodziai =zodisJunginys(zodis,zodis);
+  
+  console.log(zodis) ;
+  console.log(sujungtiZodziai)
+
+}
+function zodisJunginys(zodis,zodis) 
+{ return zodis+zodis
     
+     
+
 }
