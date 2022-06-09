49add1b53845250afd968cd62a2b8649964e7cf5
audriusxp <palm@inbox.lt>
2022-06-09 14:21:42 +0300

    pabaigta uzduotis 2

--- script.js
+++ script.js
@@ -1,12 +1,19 @@
 window.onload = function() {
   let zodis = "kelmas";
-  let sujungtiZodziai =zodisJunginys(zodis,zodis);
+  let zodis1 = "rastas"
+  let zodis2 = "saka"
+  let sujungtiZodziai =zodisJunginys(zodis);
+  let sujungtiZodziai1 =zodisJunginys(zodis1);
+  let sujungtiZodziai2 = zodisJunginys(zodis2)
   
   console.log(zodis) ;
+  console.log(zodis1);
   console.log(sujungtiZodziai)
+  console.log(sujungtiZodziai1)
+  console.log(sujungtiZodziai2)
 
 }
-function zodisJunginys(zodis,zodis) 
+function zodisJunginys(zodis) 
 { return zodis+zodis
     
      
